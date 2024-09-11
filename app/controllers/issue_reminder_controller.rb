ff462ee56089a7e544c2f2ee26e61ec1c5ae6e5c
KeKkEmEn <46707868+KeKkEmEn@users.noreply.github.com>
2024-09-11 14:39:29 +0200

    Update issue_reminder_controller.rb

--- app/controllers/issue_reminder_controller.rb
+++ app/controllers/issue_reminder_controller.rb
@@ -1,27 +1,29 @@
 class IssueReminderController < ApplicationController
-    before_action :find_project_by_project_id
-    before_action :authorize
-  
-    def index
-      @issues = find_reminder_issues
-    end
-  
-    def send_reminders
-      issues = find_reminder_issues
-      issues.each do |issue|
-        IssueReminderMailer.issue_reminder(issue).deliver_now
-      end
-      redirect_to project_issue_reminder_path(@project), notice: l(:notice_reminders_sent)
-    end
-  
-    private
-  
-    def find_reminder_issues
-      @project.issues.open.where('updated_on < ?', reminder_threshold_date)
-    end
-  
-    def reminder_threshold_date
-      Setting.plugin_redmine_issue_reminder['reminder_days'].to_i.days.ago
-    end
+  before_action :find_project, :authorize
+
+  def index
+    @issues = find_reminder_issues
   end
-  
+
+  def send_reminders
+    issues = find_reminder_issues
+    issues.each do |issue|
+      IssueReminderMailer.issue_reminder(issue).deliver_now
+    end
+    redirect_to project_issue_reminder_path(@project), notice: l(:notice_reminders_sent)
+  end
+
+  private
+
+  def find_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def find_reminder_issues
+    @project.issues.open.where('updated_on < ?', reminder_threshold_date)
+  end
+
+  def reminder_threshold_date
+    Setting.plugin_redmine_issue_reminder['reminder_days'].to_i.days.ago
+  end
+end
