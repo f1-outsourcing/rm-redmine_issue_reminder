81535e4821735ef358d1bbc2b622b579a2c7d8b7
KeKkEmEn <46707868+KeKkEmEn@users.noreply.github.com>
2024-09-11 14:13:48 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-    resources :projects do
-      resource :issue_reminder, only: [:index], controller: 'issue_reminder' do
-        post 'send_reminders', on: :collection
-      end
+  resources :projects do
+    resource :issue_reminder, only: [:index], controller: 'issue_reminder' do
+      post 'send_reminders', on: :collection
     end
   end
-  
+end
