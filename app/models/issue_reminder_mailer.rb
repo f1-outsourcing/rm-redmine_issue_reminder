f113fdcc925af2de8c36da5ed22e28a429fc9797
KeKkEmEn <46707868+KeKkEmEn@users.noreply.github.com>
2024-09-11 15:37:42 +0200

    Update issue_reminder_mailer.rb

--- app/models/issue_reminder_mailer.rb
+++ app/models/issue_reminder_mailer.rb
@@ -1,10 +1,10 @@
 class IssueReminderMailer < Mailer
-    def issue_reminder(issue)
-      @issue = issue
-      @reminder_text = Setting.plugin_redmine_issue_reminder['reminder_text']
-      mail(to: issue.assigned_to.mail,
-           subject: "#{l(:label_reminder)}: #{issue.subject}",
-           from: Setting.mail_from)
-    end
+  def self.issue_reminder(issue)
+    to = issue.assigned_to.is_a?(Group) ? issue.assigned_to.users : issue.assigned_to
+    @issue = issue
+    @reminder_text = Setting.plugin_redmine_issue_reminder['reminder_text']
+    mail(to: to.map(&:mail),
+         subject: "#{l(:label_reminder)}: #{issue.subject}",
+         from: Setting.mail_from)
   end
-  
+end
