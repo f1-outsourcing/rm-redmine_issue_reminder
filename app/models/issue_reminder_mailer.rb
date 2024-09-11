2ea67c438c24dadfbe6911563c84e0f200c4129a
KeKkEmEn <46707868+KeKkEmEn@users.noreply.github.com>
2024-09-11 16:25:24 +0200

    Update issue_reminder_mailer.rb

--- app/models/issue_reminder_mailer.rb
+++ app/models/issue_reminder_mailer.rb
@@ -1,10 +1,11 @@
 class IssueReminderMailer < Mailer
-  def self.issue_reminder(issue)
+  def self.issue_reminder(issue, days_before_due)
     to = issue.assigned_to.is_a?(Group) ? issue.assigned_to.users : issue.assigned_to
     @issue = issue
+    @days_before_due = days_before_due
     @reminder_text = Setting.plugin_redmine_issue_reminder['reminder_text']
     mail(to: to.map(&:mail),
-         subject: "#{l(:label_reminder)}: #{issue.subject}",
+         subject: "#{l(:label_reminder)}: #{issue.subject} (Due in #{days_before_due} days)",
          from: Setting.mail_from)
   end
 end
