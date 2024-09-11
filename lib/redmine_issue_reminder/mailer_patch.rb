800c57e27024b41a1e19c695b7584af6219106b8
KeKkEmEn <46707868+KeKkEmEn@users.noreply.github.com>
2024-09-11 13:58:09 +0200

    Update mailer_patch.rb

--- lib/redmine_issue_reminder/mailer_patch.rb
+++ lib/redmine_issue_reminder/mailer_patch.rb
@@ -1,21 +1,20 @@
 module RedmineIssueReminder
-    module MailerPatch
-      def self.included(base)
-        base.send(:include, InstanceMethods)
-        base.class_eval do
-          alias_method :issue_reminder, :issue_reminder
-        end
+  module MailerPatch
+    def self.included(base)
+      base.send(:include, InstanceMethods)
+      base.class_eval do
+        alias_method :issue_reminder, :issue_reminder
       end
-  
-      module InstanceMethods
-        def issue_reminder(issue)
-          IssueReminderMailer.issue_reminder(issue)
-        end
+    end
+
+    module InstanceMethods
+      def issue_reminder(issue)
+        IssueReminderMailer.issue_reminder(issue)
       end
     end
   end
-  
-  Rails.configuration.to_prepare do
-    Mailer.send(:include, RedmineIssueReminder::MailerPatch)
-  end
-  
+end
+
+Rails.configuration.to_prepare do
+  Mailer.send(:include, RedmineIssueReminder::MailerPatch)
+end
